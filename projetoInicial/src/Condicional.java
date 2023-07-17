de431f91fdf3d64b71a4ac7a6c944958cf9d111f
andrelrocha <andrerocha0911@gmail.com>
2023-07-17 17:00:14 -0300

    capturando input do usuário e formatando string

--- projetoInicial/src/Condicional.java
+++ projetoInicial/src/Condicional.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Condicional {
     public static void main(String[] args) {
         double nota1 = 8.3;
@@ -13,9 +15,27 @@ public class Condicional {
         }
 
         if (passou) {
-            System.out.printf("Parabens, voce passou de ano.\nSua nota foi %.2f", media);
+            System.out.printf("Parabens, voce passou de ano.\nSua nota foi %.2f\n", media);
         } else {
-            System.out.println("Infelizmente não deu certo pra ti");
+            System.out.println("Infelizmente não deu certo pra ti\n");
         }
+
+        ////////////////////////
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nomeManga = "Berserk";
+        String nomeMangaDois = "Vagabond";
+
+        System.out.println("Qual o melhor manga da historia?");
+        String escolha = scanner.nextLine();
+        String escolhaFormatada = escolha.substring(0,1).toUpperCase() + escolha.substring(1);
+
+        if (escolhaFormatada.equals(nomeManga) || escolhaFormatada.equals(nomeMangaDois)) {
+            System.out.println("Acertou!");
+        } else {
+            System.out.printf("Como assim %s. Você deveria ler Berserk", escolhaFormatada);
+        }
+
     }
 }
