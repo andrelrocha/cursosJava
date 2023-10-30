6d3a0a7110c3435f900f33d2d00190d906e49d19
andrelrocha <andrerocha0911@gmail.com>
2023-10-30 11:23:47 -0300

    injetando dependendcias no hub para o user access

--- exercicios-poo/trabalho_3_json/demo/src/test/java/rocha/andre/json/Hub.java
+++ exercicios-poo/trabalho_3_json/demo/src/test/java/rocha/andre/json/Hub.java
@@ -3,6 +3,8 @@ package rocha.andre.json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Hub {
     @Autowired
@@ -13,14 +15,69 @@ public class Hub {
     private Carrinho carrinho;
 
     public static void main(String[] args) {
-        /*
-        var criarProduto = new CriarProduto();
-        criarProduto.criarProduto();
+        Scanner scanner = new Scanner(System.in);
+        var user = scanner.nextLine();
 
-        var editarProduto = new EditarProduto();
-        editarProduto.editarProduto();
-        */
-         var carrinho = new Carrinho();
-         carrinho.executar();
+        if (user.equals("administrador")) {
+            while(true) {
+                System.out.println("Digite o numero da operação desejada ou 'sair'\n");
+                System.out.println();
+                var esc = scanner.nextInt();
+
+                switch (esc) {
+                    case 1:
+                        var criarProduto = new CriarProduto();
+                        criarProduto.criarProduto();
+                        break;
+
+                    case 2:
+                        var editarProduto = new EditarProduto();
+                        editarProduto.editarProduto();
+                        break;
+
+                    case 3:
+                        //CASE PARA DELETAR UM PRODUTO
+                        System.out.println("Opção 3 selecionada.");
+                        break;
+
+                    case 4:
+                        //CASE PARA LISTAR COMPRAS
+                        System.out.println("Opção 3 selecionada.");
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida.");
+                        break;
+                }
+            }
+        }
+
+        if (user.equals("cliente")) {
+            System.out.println("Digite o numero da operação desejada ou 'sair'\n");
+            System.out.println();
+            var esc = scanner.nextInt();
+
+            switch (esc) {
+                case 1:
+                    var carrinho = new Carrinho();
+                    carrinho.executar();
+                    break;
+
+                case 2:
+                    //
+                    break;
+
+                case 3:
+                    //
+                    System.out.println("Opção 3 selecionada.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+
+
     }
 }
